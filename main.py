5bd8e24b85f0f585bd02cfe35e678f76a25fb6c1
Equinox2654 <thibishaan@gmail.com>
2025-05-18 17:59:16 +1000

    added basic game loop functionality

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+#Main Game Loop
+while constants.running:
+    # Initialising pygame
+    pygame.init()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            constants.running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+
+pygame.quit()
