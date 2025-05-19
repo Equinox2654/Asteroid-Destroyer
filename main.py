d9e5a0c63bbb41a652a142f0bf8204fb35e8f27d
Equinox2654 <thibishaan@gmail.com>
2025-05-19 13:06:59 +1000

    added player rotation: but too fast, need to fix later

--- main.py
+++ main.py
@@ -10,27 +10,29 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-dt = 0
-
-#Main Game Loop
-while constants.running:
-    # Initialising pygame
-    pygame.init()
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            constants.running = False
-
-    screen.fill((0, 0, 0))
-
-    """Game Code below here"""
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    dt = 0
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
-    player.draw(screen)
 
-    dt = pygame.time.get_ticks() / 1000.0
+    #Main Game Loop
+    while constants.running:
+        # Initialising pygame
+        pygame.init()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                constants.running = False
 
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+
+        """Game Code below here"""
+        player.update(dt)
+        player.draw(screen)
+
+        #pygame.time.Clock.tick(60)  # Limit to 60 FPS
+        dt = pygame.time.get_ticks() / 1000.0
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
